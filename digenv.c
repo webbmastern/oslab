a6e82a92837a19b00d34fea4bc494e7e28438202
montao <you@example.com>
2015-04-08 20:11:41 +0200

    small bugfix removing unnecessary test

--- digenv.c
+++ digenv.c
@@ -28,15 +28,13 @@ int spawn_proc (int in, int out, struct command *cmd) {
 int fork_pipes (int n, struct command *cmd) {
     int i;
     int in, fd [2];
-    in = 0;
     for (i = 0; i < n - 1; ++i) {
         pipe (fd);
         spawn_proc (in, fd [1], cmd + i);
         close (fd [1]);
         in = fd [0];
     }
-    if (in != 0)
-        dup2 (in, 0);
+    dup2 (in, 0);
     return execvp (cmd [i].argv [0], (char * const *)cmd [i].argv);
 }
 
@@ -51,7 +49,7 @@ int main (int argc, char ** argv) {
     }
     if (argc > 1) { /* I'd like an argument */
 
-        if (strncmp(argv[1], "cd", 2)) {
+        if (strncmp(argv[1], "cd", 2) && strncmp(argv[1], "exit", 2)) {
             char *tmp;
             int len = 1;
             for( i=1; i<argc; i++)
@@ -72,9 +70,12 @@ int main (int argc, char ** argv) {
             struct command cmd [] = { {printenv}, {grep}, {sort}, {less} };
             return fork_pipes (4, cmd);
             free(tmp);
-        } else { /* change directory */
+        } else if (! strncmp(argv[1], "cd", 2)) { /* change directory */
             printf("change directory to %s\n" , argv[2]);
             chdir(argv[2]);
+        } else if (! strncmp(argv[1], "exit", 2)) { /* change directory */
+            printf("exit\n");
+            exit(0);
         }
     }
     exit(0);
