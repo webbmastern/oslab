d93fca968f5970b1637b399d3b941899811ee589
montao <you@example.com>
2015-04-12 19:00:55 +0200

    improved error handling

--- digenv.c
+++ digenv.c
@@ -1,92 +1,121 @@
-#include <sys/types.h>
 #include <errno.h>
+#include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <string.h>
+#include <unistd.h>
+
 struct command
 {
-    const char **argv;
+    char * const *argv;
 };
+
+static _Noreturn void err_syserr(char *fmt, ...)
+{
+    int errnum = errno;
+    va_list args;
+    va_start(args, fmt);
+    vfprintf(stderr, fmt, args);
+    va_end(args);
+    if (errnum != 0)
+        fprintf(stderr, "(%d: %s)\n", errnum, strerror(errnum));
+    exit(EXIT_FAILURE);
+}
+
 /* Helper function that spawns processes */
-int spawn_proc (int in, int out, struct command *cmd) {
+static int spawn_proc(int in, int out, struct command *cmd)
+{
     pid_t pid;
-    if ((pid = fork ()) == 0) {
-        if (in != 0) {
-            /*if (dup2(in, 0) == -1) {
-                perror("dup2 failed");
-                exit(1);
-            }*/
-            dup2 (in, 0);
-            close (in);
+    if ((pid = fork()) == 0)
+    {
+        if (in != 0)
+        {
+            if (dup2(in, 0) < 0)
+                err_syserr("dup2() failed on stdin for %s: ", cmd->argv[0]);
+            ;
+            close(in);
         }
-        if (out != 1) {
-            dup2 (out, 1);
-            close (out);
+        if (out != 1)
+        {
+            if (dup2(out, 1) < 0)
+                err_syserr("dup2() failed on stdout for %s: ", cmd->argv[0]);
+            close(out);
         }
-        if (execvp(cmd->argv [0], (char * const *)cmd->argv) < 0) {
-            perror("execvp failed");
-            exit(1);
-        }
-    } else if (pid < 0) {
-        perror("fork failed");
-        exit(1);
+        fprintf(stderr, "%d: executing %s\n", (int)getpid(), cmd->argv[0]);
+        execvp(cmd->argv[0], cmd->argv);
+        err_syserr("failed to execute %s: ", cmd->argv[0]);
     }
+    else if (pid < 0)
+        err_syserr("fork failed: ");
     return pid;
 }
+
 /* Helper function that forks pipes */
-int fork_pipes (int n, struct command *cmd) {
+static void fork_pipes(int n, struct command *cmd)
+{
     int i;
-    int in, fd [2];
-    for (i = 0; i < n - 1; ++i) {
-        pipe (fd);
-        spawn_proc (in, fd [1], cmd + i);
-        close (fd [1]);
-        in = fd [0];
+    int in = 0;
+    int fd[2];
+    for (i = 0; i < n - 1; ++i)
+    {
+        pipe(fd);
+        spawn_proc(in, fd[1], cmd + i);
+        close(fd[1]);
+        in = fd[0];
     }
-    dup2 (in, 0);
-    return execvp (cmd [i].argv [0], (char * const *)cmd [i].argv);
+    if (dup2(in, 0) < 0)
+        err_syserr("dup2() failed on stdin for %s: ", cmd[i].argv[0]);
+    fprintf(stderr, "%d: executing %s\n", (int)getpid(), cmd[i].argv[0]);
+    execvp(cmd[i].argv[0], cmd[i].argv);
+    err_syserr("failed to execute %s: ", cmd[i].argv[0]);
 }
 
-int main (int argc, char ** argv) {
+int main(int argc, char **argv)
+{
     int i;
-    if (argc == 1) { /* There were no arguments */
-        const char *printenv[] = { "printenv", 0};
-        const char *sort[] = { "sort", 0 };
-        const char *less[] = { "less", 0 };
-        struct command cmd [] = { {printenv}, {sort}, {less} };
-        return fork_pipes (3, cmd);
+    if (argc == 1)   /* There were no arguments */
+    {
+        char *printenv[] = { "printenv", 0};
+        char *sort[] = { "sort", 0 };
+        char *less[] = { "less", 0 };
+        struct command cmd[] = { {printenv}, {sort}, {less} };
+        fork_pipes(3, cmd);
     }
-    if (argc > 1) { /* I'd like an argument */
-
-        if (strncmp(argv[1], "cd", 2) && strncmp(argv[1], "exit", 2)) {
-            char *tmp;
-            int len = 1;
-            for( i=1; i<argc; i++)
-            {
-                len += strlen(argv[i]) + 2;
-            }
-            tmp = (char*) malloc(len);
-            tmp[0] = '\0';
-            int pos = 0;
-            for( i=1; i<argc; i++)
-            {
-                pos += sprintf(tmp+pos, "%s%s", (i==1?"":"|"), argv[i]);
-            }
-            const char *printenv[] = { "printenv", 0};
-            const char *grep[] = { "grep", "-E", tmp, NULL};
-            const char *sort[] = { "sort", 0 };
-            const char *less[] = { "less", 0 };
-            struct command cmd [] = { {printenv}, {grep}, {sort}, {less} };
-            return fork_pipes (4, cmd);
-            free(tmp);
-        } else if (! strncmp(argv[1], "cd", 2)) { /* change directory */
-            printf("change directory to %s\n" , argv[2]);
+    else
+    {
+        if (strcmp(argv[1], "cd") == 0)      /* change directory */
+        {
+            printf("change directory to %s\n", argv[2]);
             chdir(argv[2]);
-        } else if (! strncmp(argv[1], "exit", 2)) { /* change directory */
+        }
+        else if (strcmp(argv[1], "exit") == 0)
+        {
             printf("exit\n");
             exit(0);
         }
+        else
+        {
+            char *tmp;
+            int len = 1;
+            for (i = 1; i < argc; i++)
+            {
+                len += strlen(argv[i]) + 2;
+            }
+            tmp = (char *) malloc(len);
+            tmp[0] = '\0';
+            int pos = 0;
+            for (i = 1; i < argc; i++)
+            {
+                pos += sprintf(tmp + pos, "%s%s", (i == 1 ? "" : "|"), argv[i]);
+            }
+            char *printenv[] = { "printenv", 0};
+            char *grep[] = { "grep", "-E", tmp, NULL};
+            char *sort[] = { "sort", 0 };
+            char *less[] = { "less", 0 };
+            struct command cmd[] = { {printenv}, {grep}, {sort}, {less} };
+            fork_pipes(4, cmd);
+            free(tmp);
+        }
     }
-    exit(0);
+    return(0);
 }
