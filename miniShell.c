ef4e53581627d6a85d620cedc631168e4dac30e3
montao <you@example.com>
2015-05-07 20:56:48 +0200

    Implemented custom commands

--- miniShell.c
+++ miniShell.c
@@ -2,14 +2,10 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
-#include<dirent.h>
-#include<error.h>
+#include <dirent.h>
 #include <errno.h>
 #include <stdarg.h>
-#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
 #include <signal.h>
 int mystrcmp(char const *, char const *);
 
@@ -38,21 +34,21 @@ static int spawn_proc(int in, int out, struct command *cmd)
         {
             if (dup2(in, 0) < 0)
                 err_syserr("dup2() failed on stdin for %s: ", cmd->argv[0]);
-                ;
+            ;
             close(in);
         }
         if (out != 1)
         {
             if (dup2(out, 1) < 0)
                 err_syserr("dup2() failed on stdout for %s: ", cmd->argv[0]);
-                close(out);
+            close(out);
         }
         fprintf(stderr, "%d: executing %s\n", (int)getpid(), cmd->argv[0]);
         execvp(cmd->argv[0], cmd->argv);
-          err_syserr("failed to execute %s: ", cmd->argv[0]);
+        err_syserr("failed to execute %s: ", cmd->argv[0]);
     }
     else if (pid < 0)	{
-         err_syserr("fork failed: "); 
+        err_syserr("fork failed: ");
     }
     return pid;
 }
@@ -71,43 +67,74 @@ static void fork_pipes(int n, struct command *cmd)
         in = fd[0];
     }
     if (dup2(in, 0) < 0)	{
-           err_syserr("dup2() failed on stdin for %s: ", cmd[i].argv[0]);
+        err_syserr("dup2() failed on stdin for %s: ", cmd[i].argv[0]);
     }
     fprintf(stderr, "%d: executing %s\n", (int)getpid(), cmd[i].argv[0]);
     execvp(cmd[i].argv[0], cmd[i].argv);
-     err_syserr("failed to execute %s: ", cmd[i].argv[0]);
+    err_syserr("failed to execute %s: ", cmd[i].argv[0]);
 }
 
 #define BUFFERSIZE 200
+
+
+#define BUFFER_LEN 1024
+
 int main() {
+    char line[BUFFER_LEN];  //get command line
+    char* argv[100];        //user command
+    char* path= "/bin/";    //set path at bin
+    char progpath[20];      //full file path
+    int argc;               //arg count
 
-    char *args[80];
-    char buffer[BUFFERSIZE];
-    char *prompt = "OS";
-    char *a = ">";
+    while(1) {
 
-    char *tok;
-    tok = strtok (buffer," ");
+        printf("miniShell>> ");                    //print shell prompt
 
-
-    while(buffer != NULL) {
-        bzero(buffer, BUFFERSIZE);
-        printf("%s%s",prompt,a);
-        fgets(buffer, BUFFERSIZE, stdin);
-        if(mystrcmp(buffer,"cd") == 0) {
-            tok = strchr(buffer,' ')+1; //use something more powerful
-            *strchr(tok, '\n')='\0';
-            cd(tok);
-        } 
-	else if(mystrcmp(buffer,"exit") == 0) {
-	   	return 0;
-	}	
-	else {
-            system("ls"); //for testing the CWD/PWD
-	    printf("Spawned foreground process: %d\n", getpid());
+        if(!fgets(line, BUFFER_LEN, stdin)) { //get command and put it in line
+            break;                                //if user hits CTRL+D break
         }
+        size_t length = strlen(line);
+        if (line[length - 1] == '\n') {
+            line[length - 1] = '\0';
+        }
+        if(strcmp(line, "exit")==0) {           //check if command is exit
+            break;
+        }
+
+        char *token;                  //split command into separate strings
+        token = strtok(line," ");
+        int i=0;
+        while(token!=NULL) {
+            argv[i]=token;
+            token = strtok(NULL," ");
+            i++;
+        }
+        argv[i]=NULL;                     //set last value to NULL for execvp
+
+        argc=i;                           //get arg count
+        for(i=0; i<argc; i++) {
+            printf("%s\n", argv[i]);      //print command/args
+        }
+        strcpy(progpath, path);           //copy /bin/ to file path
+        strcat(progpath, argv[0]);            //add program to path
+
+        for(i=0; i<strlen(progpath); i++) {   //delete newline
+            if(progpath[i]=='\n') {
+                progpath[i]='\0';
+            }
+        }
+        int pid= fork();              //fork child
+
+        if(pid==0) {              //Child
+            execvp(progpath,argv);
+            fprintf(stderr, "Child process could not do execvp\n");
+
+        } else {                   //Parent
+            wait(NULL);
+            printf("Child exited\n");
+        }
+
     }
-    return 0;
 }
 
 int mystrcmp(char const *p, char const *q)
